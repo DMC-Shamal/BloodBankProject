881b1092762e057b3f8df3a20b1b14c90339fa2a
W3-80635 <shmlrmwt@gmail.com>
2024-02-18 13:58:26 +0530

    client

---
+++ client/src/Landing.js
@@ -0,0 +1,37 @@
+// import { BrowserRouter as Router,Switch, Route } from "react-router-dom";
+// import NavBar from "./pages/NavBar";
+// import HospitalList from "./services/HospitalList";
+// import HospitalRegistration from "./services/HospitalRegistration";
+// import RegistrationConfirmation from "./pages/RegistrationConfirmation";
+// import EditProfile from "./services/EditProfile";
+
+// function Landing() {
+//     return (<>
+//     <Router>
+                
+//                 {/* <Link to="/">Home</Link> {" | "} */}
+//                 {/* <Link to="/hospitallist"></Link> */}
+//                 {/* <Link to="/hospital-registration"></Link> {" | "} */}
+               
+//                 {/* <hr></hr> */}
+//                 <Switch>
+//                     <Route exact path="/" component={NavBar}/>
+//                     <Route exact path="/navbar" component={NavBar}/>
+//                     <Route exact path="/editprofile" component={EditProfile}/>
+                   
+//                     <Route exact path="/hlist" component={HospitalList}/>
+//                     <Route exact path="/hregister" component={HospitalRegistration}/>
+//                     <Route exact path="/registration-confirmation" component={RegistrationConfirmation}/>
+//                     {/* <ProtectedRoute path="/contact" 
+//                                     component={Contact}/> */}
+                   
+//                 </Switch>
+//                 </Router>
+//                 {/* <hr></hr>
+//                 <h1>Footer</h1> */}
+//             </>);
+// }
+
+
+
+// export default Landing;
